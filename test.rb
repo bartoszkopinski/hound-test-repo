9e31ff70aa480d48c8a314bcdf376d7249adf24e
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2014-08-09 15:34:02 +0200

    test commit 4

--- test.rb
+++ test.rb
@@ -1,13 +1,13 @@
 class A_bad_named_class
 
-    def ABadNamed_Method
-        a = 2
-    if a
-      a+=1
-    return true
-    else
-      return false
-    end
-      return false
-    end
+  def ABadNamed_Method
+      a = 2
+  if a
+    a+=1
+  return true
+  else
+    return false
+  end
+    return false
+  end
 end
