ae43e76663a9846446a0bc6b58bf6c8a999e33d9
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2014-08-10 17:30:23 +0200

    test commit 11

---
+++ test_4.rb
@@ -0,0 +1,20 @@
+class a_bad_named_class
+
+  def ABadNamed_Method()
+        a = 2
+    if a
+      a+=1
+    return true
+    else
+      return false
+    end
+    return nil
+
+    a ? 'test' : ""
+  end
+end
+class contrived
+   def sample1()
+     puts ("this is a contrived example")
+      end
+    end
